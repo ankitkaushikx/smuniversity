60cfa561f8865d984c1235987cce01f369cfd5ea
ankitkaushikx <ankitkaushikx@gmail.com>
2024-06-22 23:07:30 +0530

    layout Setup

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+//Auth Routes
+require __DIR__.'/auth.php';
+
+//Dashboard Routes
+require __DIR__ .'/dashboard.php';
+
+//Front Routes
+require __DIR__ . '/front.php';
